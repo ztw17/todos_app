f2557ec68a3cabfa2759d4f150a733c5d75dcfe1
Zach Weber <weberzt@gmail.com>
2020-07-16 15:52:03 -0500

    added App.js, TodosContainer.js, and App.css

---
+++ src/components/TodosContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class TodosContainer extends Component {
+  render() {
+    return (
+      <div>
+	<div className="inputContainer">
+	  <input className="taskInput" type="text" 
+	    placeholder="Add a task" maxLength="50" />
+	</div>  	    
+	<div className="listWrapper">
+	   <ul className="taskList">
+	   </ul>
+	</div>
+      </div>    
+    )
+  }
+}
+
+export default TodosContainer
