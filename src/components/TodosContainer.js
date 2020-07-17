0d210056e382f61a9c19b3e66415413b0f4f9343
Zach Weber <weberzt@gmail.com>
2020-07-17 12:50:58 -0500

    added immutability-helper and add todo functionality

--- src/components/TodosContainer.js
+++ src/components/TodosContainer.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from 'react';
+import axios from 'axios';
+import update from 'immutability-helper';
 
 class TodosContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      todos: []
+        todos: [],
+        inputValue: ''
     }
   }
 
@@ -21,13 +23,34 @@ class TodosContainer extends Component {
     this.getTodos()
   }
 
+  handleChange = (event) => {
+    this.setState({inputValue: event.target.value});
+  }
+
+  createTodo = (event) => {
+    if (event.key === 'Enter') {
+      axios.post('/api/v1/todos', {todo: {title: event.target.value}})
+      .then(response => {
+        const todos = update(this.state.todos, {
+          $splice: [[0, 0, response.data]]
+        })
+        this.setState({
+            todos: todos,
+            inputValue: ''
+        })
+      })
+      .catch(error => console.log(error))      
+    }    
+  }
+
   render() {
     return (
       <div>
         <div className="inputContainer">
           <input className="taskInput" type="text" 
             placeholder="Add a task" maxLength="50"
-            onKeyPress={this.createTodo} />
+            onKeyPress={this.createTodo} 
+            value={this.state.inputValue} onChange={this.handleChange} />
         </div>  	    
 	<div className="listWrapper">
 	   <ul className="taskList">
