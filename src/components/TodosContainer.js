47d900bdd7a13833b4b5d004037a1dc5965def5c
Zach Weber <weberzt@gmail.com>
2020-07-18 14:45:29 -0500

    added delete functionality

--- src/components/TodosContainer.js
+++ src/components/TodosContainer.js
@@ -57,6 +57,20 @@ class TodosContainer extends Component {
     .catch(error => console.log(error))      
   }
 
+  deleteTodo = (id) => {
+    axios.delete(`/api/v1/todos/${id}`)
+    .then(response => {
+      const todoIndex = this.state.todos.findIndex(x => x.id === id)
+      const todos = update(this.state.todos, {
+        $splice: [[todoIndex, 1]]
+      })
+      this.setState({
+        todos: todos
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
   render() {
     return (
         <div>
@@ -75,7 +89,10 @@ class TodosContainer extends Component {
                                     checked={todo.done}
                                     onChange={(e) => this.updateTodo(e, todo.id)} />               
                                 <label className="taskLabel">{todo.title}</label>
-                                <span className="deleteTaskBtn">x</span>
+                                <span className="deleteTaskBtn" 
+                                    onClick={(e) => this.deleteTodo(todo.id)}>
+                                    x
+                                </span>
                             </li>
                         )       
                     })} 	    
